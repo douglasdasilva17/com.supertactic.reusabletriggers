752dcc8c7c4e964880a17e8a79c0924499ff6385
Douglas da Silva <douglasdasilva448@gmail.com>
2025-08-20 23:42:14 -0300

    Initial commit

---
+++ Runtime/TriggerZone2D.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace Supertactic.ReusableTriggers
+{
+    public class TriggerZone2D : MonoBehaviour
+    {
+        public bool OneShot;
+        private bool _isAlreadyEntered;
+        private bool _isAlreadyExited;
+
+        public string CollisionTag;
+
+        public UnityEvent OnEnter;
+        public UnityEvent OnExit;
+
+        private void OnTriggerEnter2D(Collider2D collision)
+        {
+            if (_isAlreadyEntered)
+                return;
+
+            if (!string.IsNullOrEmpty(CollisionTag) && !collision.CompareTag(CollisionTag))
+                return;
+
+            OnEnter?.Invoke();
+
+            if (OneShot)
+                _isAlreadyEntered = true;
+        }
+
+        private void OnTriggerExit2D(Collider2D collision)
+        {
+            if (_isAlreadyExited)
+                return;
+
+            if (!string.IsNullOrEmpty(CollisionTag) && !collision.CompareTag(CollisionTag))
+                return;
+
+            OnExit?.Invoke();
+
+            if (OneShot)
+                _isAlreadyExited = true;
+        }
+    }
+}
